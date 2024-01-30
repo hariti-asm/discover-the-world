199168d4b61831fb14990f9939f2978b28d99cf4
asmae hariti <haritiasmaa74@gmail.com>
2024-01-30 14:49:41 +0100

    fixing migration issues

--- app/Models/Adventure.php
+++ app/Models/Adventure.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Adventure extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'publication_date'
+    ];
 }
--- database/migrations/2024_01_30_131321_create_images_table.php
+++ database/migrations/2024_01_30_131321_create_images_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("image_id");
+            $table->string('path');
             $table->timestamps();
         });
     }
